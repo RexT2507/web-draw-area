d1cc5dddb7bca4d32ca121609e0cc5b5109e3eb6
RexT2507 <baptiste.marmier@ynov.com>
2020-12-11 16:22:48 +0100

    [DEV] Ajout du chemin

--- back/app.js
+++ back/app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const urlPrefix = process.env.URL_PREFIX;
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
@@ -19,8 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(`${urlPrefix}/`, indexRouter);
+app.use(`${urlPrefix}/users`, usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
