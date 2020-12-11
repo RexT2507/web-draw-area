aabc70ee6d2e7e9763b8fc48e7c6b41c95db8792
Pierre ROMAN <flaeur@gmail.com>
2020-12-11 15:09:37 +0100

    [Add][Dev] Ajout de la drawZone rudimentairement fonctionnel

--- front/web-draw-area/src/app/app.module.ts
+++ front/web-draw-area/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DrawZoneComponent } from './component/draw-zone/draw-zone.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DrawZoneComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front/web-draw-area/src/app/component/draw-zone/draw-zone.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DrawZoneComponent } from './draw-zone.component';
+
+describe('DrawZoneComponent', () => {
+  let component: DrawZoneComponent;
+  let fixture: ComponentFixture<DrawZoneComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DrawZoneComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DrawZoneComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
