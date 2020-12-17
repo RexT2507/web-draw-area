d4f460625547bcc7a73ad68a531096d4ebcd4349
RexT2507 <baptiste.marmier@ynov.com>
2020-12-17 17:01:05 +0100

    [DEV] Ajout de CORS

--- back/app.js
+++ back/app.js
@@ -4,16 +4,23 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const cors = require('cors');
+
 const urlPrefix = process.env.URL_PREFIX;
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const { cpuUsage } = require('process');
 
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(
+  cors()
+);
 
 app.use(logger('dev'));
 app.use(express.json());
